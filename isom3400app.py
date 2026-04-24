2e3deb43cfce097e3edbfca0b9e2b0e056123a66
ryutalung <sheklunggor@gmail.com>
2026-04-24 12:54:25 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -7,7 +7,7 @@ st.header("Manager Input Section")
 st.write("Please enter the monthly sales target and enter the region.")
 target = st.number_input("Enter monthly sales target (USD):", min_value = 0, value = 0)
 if target > 100000:
-  st.write("Great! You have enter an ambitious target!")
+  st.write("Great! You have entered an ambitious target!")
 region = st.selectbox("Select region:", ["East", "South", "West", "North"])
 
 if st.button("Submit"):
