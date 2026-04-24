5052fdda537cacbe525994f8fc1fe75d509c86f8
ryutalung <sheklunggor@gmail.com>
2026-04-24 12:48:41 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -6,7 +6,7 @@ st.title("Retail Business Dashboard")
 st.header("Manager Input Section")
 st.write("Please enter the monthly sales target and enter the region.")
 target = st.number_input("Enter monthly sales target (USD):", min_value = 0, value = 0)
-region = st.button("Select region:", ["East", "South", "West", "North"])
+region = st.selectbox("Select region:", ["East", "South", "West", "North"])
 
 if st.button("Submit"):
   st.write("You submit!")
