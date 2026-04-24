e10f324a1fae067a929b0e7609ec37c85beb8f2e
ryutalung <sheklunggor@gmail.com>
2026-04-24 12:48:24 +0800

    Update isom3400app.py

--- isom3400app.py
+++ isom3400app.py
@@ -8,7 +8,7 @@ st.write("Please enter the monthly sales target and enter the region.")
 target = st.number_input("Enter monthly sales target (USD):", min_value = 0, value = 0)
 region = st.button("Select region:", ["East", "South", "West", "North"])
 
-if button("Submit"):
+if st.button("Submit"):
   st.write("You submit!")
   st.success("Complete")
   
